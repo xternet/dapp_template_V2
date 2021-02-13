2ddedb1546f7935b23d7d1b17f3a9159d62c6f25
xternet <majewski.post@gmail.com>
2021-02-13 16:39:27 +0100

    .

--- src/backEnd/contracts/Contract.sol
+++ src/backEnd/contracts/Contract.sol
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.6.12;
+pragma solidity ^0.8.0;
 
 contract Contract {
   string public name;
 
-  constructor() public {
+  constructor() {
   	name = "ContractName";
   }
 
